b8a06b31de2b6ee41629a0ded6693e2ed79db956
Alexei Bushuev <alexeib.2019@gmail.com>
2019-09-03 14:11:06 +0300

    added schema endpoint

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
     url(r'^', include('tutorial.snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('schema/', schema_view),
 ]
